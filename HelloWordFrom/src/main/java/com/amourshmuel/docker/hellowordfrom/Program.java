2a0c80ec3278f08f347ab701b8e724fe6a95e86f
amourshmuel <amour.shmuel@gmail.com>
2016-08-29 13:05:37 +0300

    Add Project

---
+++ HelloWordFrom/src/main/java/com/amourshmuel/docker/hellowordfrom/Program.java
@@ -0,0 +1,14 @@
+package com.amourshmuel.docker.hellowordfrom;
+
+/**
+ * Created by Shmuel on 8/29/2016, Project: HelloWordFrom.
+ */
+public class Program {
+
+    public static void main(String[] args){
+        if(args.length>0)
+            System.out.print("Hello World from: "+ args[0].toString() );
+        else
+            System.out.print("Hello World from: Shmuel");
+    }
+}
